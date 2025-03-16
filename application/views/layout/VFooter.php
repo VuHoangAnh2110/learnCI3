30df6af167f60d175ce8c80e661f81af425e3a4c
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2025-03-16 23:01:42 +0700

    Create VFooter.php

---
+++ application/views/layout/VFooter.php
@@ -0,0 +1,16 @@
+
+<div class="mb-0">
+    <footer class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto text-center">
+            <p>&copy; 2024 - Học với codeigniter 3</p>
+        </div>
+    </footer>
+</div>
+
+
+    <script src="{$base_url}application/assets/toastr/jquery.js"></script>
+    <script src="{$base_url}application/assets/toastr/toastr.min.js"></script>
+    <script src="{$base_url}application/assets/index.js"></script>
+
+</body>
+</html>
